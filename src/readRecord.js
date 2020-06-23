3c0f9bedf4308b1299b6ef4d4a91545f2914603c
TERRY WONG <iamtktwong@gmail.com>
2020-06-23 13:08:01 +0800

    update

--- src/readRecord.js
+++ src/readRecord.js
@@ -1,7 +1,19 @@
 const readRecord = (event, _, callback) => {
   const kinesisRecords = event.Records || [];
+  kinesisRecords.forEach(item => {
+    console.log(`partitionKey: ${item.kinesis.partitionKey}`);
+    console.log(`kinesisSchemaVersion: ${item.kinesis.kinesisSchemaVersion}`);
+    console.log(`data: ${Buffer(item.kinesis.data, 'base64').toString('ASCII')}`);
+    console.log(`eventSource: ${item.eventSource}`);
+    console.log(`eventID: ${item.eventID}`);
+    console.log(`invokeIdentityArn: ${item.invokeIdentityArn}`);
+    console.log(`eventVersion: ${item.eventVersion}`);
+    console.log(`eventName: ${item.eventName}`);
+    console.log(`eventSourceARN: ${item.eventSourceARN}`);
+    console.log(`eventVersion: ${item.eventVersion}`);
+  });
   if (kinesisRecords) {
-    console.log('Retrieved Records', JSON.stringify(kinesisRecords));
+    // console.log('Retrieved Records', JSON.stringify(kinesisRecords));
     callback(null, kinesisRecords);
   }
   callback(null);
